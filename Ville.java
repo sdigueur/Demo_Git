fe48e7909e5ac2b890e19ff745eefaf23089389d
sdigueur <sdigueur@gmail.com>
2019-09-22 17:15:06 +0200

    vs

--- Ville.java
+++ Ville.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Set;
 
 public class Ville {
   private String nomVille;
@@ -10,12 +11,73 @@ public class Ville {
   private List<String> mairieList;
   private List<String> tahanList;
 
-  public Ville() {
-    System.out.println("Création d'une ville !");
+  private Set<String> gouvSet;
+  private Set<String> mairieSet;
+  private Set<String> tahanSet;
+
+
+        public Ville() {
+          System.out.println("Création d'une ville !");
+          nomVille = "Inconnu";
+          nomPays = "Inconnu";
+          nbreHabitants = 0;
+          this.setCategorie();
+        }
+
+  public void maVille() {
+              System.out.println("Création d'une ville !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!   aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     nomVille = "Inconnu";
     nomPays = "Inconnu";
     nbreHabitants = 0;
     this.setCategorie();
+  }
+
+  public List<String> getGouvList() {
+    return gouvList;
+  }
+
+  public void setGouvList(List<String> gouvList) {
+    this.gouvList = gouvList;
+  }
+
+  public List<String> getMairieList() {
+    return mairieList;
+  }
+
+  public void setMairieList(List<String> mairieList) {
+    this.mairieList = mairieList;
+  }
+
+  public List<String> getTahanList() {
+    return tahanList;
+  }
+
+  public void setTahanList(List<String> tahanList) {
+    this.tahanList = tahanList;
+  }
+
+  public Set<String> getGouvSet() {
+    return gouvSet;
+  }
+
+  public void setGouvSet(Set<String> gouvSet) {
+    this.gouvSet = gouvSet;
+  }
+
+  public Set<String> getMairieSet() {
+    return mairieSet;
+  }
+
+  public void setMairieSet(Set<String> mairieSet) {
+    this.mairieSet = mairieSet;
+  }
+
+  public Set<String> getTahanSet() {
+    return tahanSet;
+  }
+
+  public void setTahanSet(Set<String> tahanSet) {
+    this.tahanSet = tahanSet;
   }
 
   public Ville(String pNom, int pNbre, String pPays)
